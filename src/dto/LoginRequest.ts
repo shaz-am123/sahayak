795f9efe9b1f1c55070b24e3014f2c3c05aa6b8b
shubham <shubham.prasad10101@gmail.com>
2024-03-27 15:02:52 +0530

    Add validator annotations

--- src/dto/LoginRequest.ts
+++ src/dto/LoginRequest.ts
@@ -1,6 +1,17 @@
-interface LoginRequest {
+import { IsNotEmpty } from "class-validator";
+
+class LoginRequest {
+    @IsNotEmpty({ message: 'Username is required' })
     username: string;
+
+    @IsNotEmpty({ message: 'Password is required' })
     password: string;
+
+    constructor(username: string, password: string)
+    {
+        this.username = username;
+        this.password = password;
+    }
 }
 
 export default LoginRequest
--- src/dto/RegistrationRequest.ts
+++ src/dto/RegistrationRequest.ts
@@ -1,7 +1,41 @@
-interface RegistrationRequest {
-    name: string;
-    username: string;
-    password: string;
+import {
+  IsNotEmpty,
+  Matches,
+  MaxLength,
+  MinLength,
+  validate,
+} from "class-validator";
+import { CustomValidationError } from "../../errors/CustomValidationError";
+
+class RegistrationRequest {
+  @IsNotEmpty({ message: "Name is required" })
+  name: string;
+
+  @IsNotEmpty({ message: "Username is required" })
+  @MinLength(4, { message: "Username must be at least 4 characters long" })
+  @MaxLength(30, { message: "Username must be at most 30 characters long" })
+  username: string;
+
+  @IsNotEmpty({ message: "Password is required" })
+  @MinLength(8, { message: "Password must be at least 8 characters long" })
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/, {
+    message:
+      "Password must contain at least one uppercase letter, one lowercase letter, and one number",
+  })
+  password: string;
+
+  constructor(name: string, username: string, password: string) {
+    this.name = name;
+    this.username = username;
+    this.password = password;
+  }
+
+  async validateRequest() {
+    const validationErrors = await validate(this);
+    if (validationErrors.length > 0) {
+        throw new CustomValidationError("Validation error: ", validationErrors)
+    }
+  }
 }
 
-export default RegistrationRequest
+export default RegistrationRequest;
