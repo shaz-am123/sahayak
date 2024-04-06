1fbf42c49995f7e541344efe91095db032d83de4
shubham <shubham.prasad10101@gmail.com>
2024-04-06 10:24:03 +0530

    DRY while handling errors across the project

--- src/dto/ExpenseCategoryRequest.ts
+++ src/dto/ExpenseCategoryRequest.ts
@@ -1,10 +1,13 @@
 import { IsNotEmpty, MaxLength, MinLength, validate } from "class-validator";
-import { CustomValidationError } from "../errors/CustomValidationError";
 
 class ExpenseCategoryRequest {
-  @IsNotEmpty({message: "Expense-category name is required"})
-  @MinLength(4, { message: "Expense-category name must be at least 2 characters long" })
-  @MaxLength(30, { message: "Expense-category name must be at most 20 characters long" })
+  @IsNotEmpty({ message: "Expense-category name is required" })
+  @MinLength(4, {
+    message: "Expense-category name must be at least 2 characters long",
+  })
+  @MaxLength(30, {
+    message: "Expense-category name must be at most 20 characters long",
+  })
   name: string;
 
   description: string;
@@ -12,13 +15,6 @@ class ExpenseCategoryRequest {
   constructor(data: { name: string; description: string }) {
     this.name = data.name;
     this.description = data.description;
-  }
-
-  async validateRequest() {
-    const validationErrors = await validate(this);
-    if (validationErrors.length > 0) {
-      throw new CustomValidationError("Validation error: ", validationErrors);
-    }
   }
 }
 
---
+++ src/dto/ValidateRequestDto.ts
@@ -0,0 +1,11 @@
+import { validate } from "class-validator";
+import { CustomValidationError } from "../errors/CustomValidationError";
+
+async function validateRequest(dto: Object) {
+    const validationErrors = await validate(dto);
+    if (validationErrors.length > 0) {
+      throw new CustomValidationError("Validation error: ", validationErrors);
+    }
+  }
+
+export default validateRequest;  
