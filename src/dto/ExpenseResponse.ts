5e7179149cb9f0dcca7aee4113d52cf6bb427ae5
shubham <shubham.prasad10101@gmail.com>
2024-04-06 21:30:46 +0530

    Add userId field to ExpenseResponse

--- src/dto/ExpenseResponse.ts
+++ src/dto/ExpenseResponse.ts
@@ -2,6 +2,7 @@ import Currency from "../enums/Currency";
 
 class ExpenseRequest {
   id: string;
+  userId: string;
   amount: number;
   currency: Currency;
   expenseCategoryId: string;
@@ -10,6 +11,7 @@ class ExpenseRequest {
 
   constructor(data: {
     id: string;
+    userId: string;
     amount: number;
     expenseCategoryId: string;
     description: string;
@@ -17,6 +19,7 @@ class ExpenseRequest {
     currency: Currency;
   }) {
     this.id = data.id;
+    this.userId = data.userId;
     this.amount = data.amount;
     this.currency = data.currency;
     this.expenseCategoryId = data.expenseCategoryId;
