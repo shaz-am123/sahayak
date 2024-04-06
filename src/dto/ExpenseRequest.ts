315ab92529e1985ff14a6a8cabdea14e4b472dd0
shubham <shubham.prasad10101@gmail.com>
2024-04-06 21:30:20 +0530

    Add error messages

--- src/dto/ExpenseRequest.ts
+++ src/dto/ExpenseRequest.ts
@@ -1,20 +1,24 @@
-import { IsNumber, IsString, IsDate, IsEnum } from 'class-validator';
+import { IsNumber, IsString, IsDate, IsEnum, IsNotEmpty } from 'class-validator';
 import Currency from '../enums/Currency';
 
 class ExpenseRequest {
-  @IsNumber()
+  @IsNotEmpty({ message: 'Amount must not be empty' })
+  @IsNumber({}, { message: 'Amount must be a number' })
   amount: number;
 
-  @IsEnum(Currency)
+  @IsNotEmpty({ message: 'Currency must not be empty' })
+  @IsEnum(Currency, { message: 'Invalid currency' })
   currency: Currency;
 
-  @IsString()
+  @IsNotEmpty({ message: 'Expense category ID must not be empty' })
+  @IsString({ message: 'Expense category ID must be a string' })
   expenseCategoryId: string;
 
-  @IsString()
+  @IsString({ message: 'Description must be a string' })
   description: string;
 
-  @IsDate()
+  @IsNotEmpty({ message: 'Date must not be empty' })
+  @IsDate({ message: 'Invalid date' })
   date: Date;
 
   constructor(data:{amount: number, expenseCategoryId: string, description: string, date: Date, currency: Currency}) {
