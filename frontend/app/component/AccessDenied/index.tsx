da549b2541a6c357a69543627fd1240a5f886fda
shubham <shubham.prasad10101@gmail.com>
2024-04-30 22:50:54 +0530

    Add button to navigate to login page from access denied page

--- frontend/app/component/AccessDenied/index.tsx
+++ frontend/app/component/AccessDenied/index.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import { Image } from "primereact/image";
 import styles from "./styles.module.scss";
+import { Button } from "primereact/button";
+import { useRouter } from "next/navigation";
 
 export default function AccessDenied() {
+  const router = useRouter();
   return (
     <>
+      <Button
+        data-testid="login-button"
+        label="Login to Access page"
+        className={styles.loginButton}
+        onClick={() => router.push("/")}
+      />
       <Image
         src="/assets/access-denied.png"
         alt="access-denied-blob"
